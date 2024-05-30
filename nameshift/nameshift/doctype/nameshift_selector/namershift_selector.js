d4261f7d93d1b4a9665fcc56828f03065308476b
MDALAMIN2526 <amin252646@gmail.com>
2024-05-30 15:10:44 +0600

    update v1.01

--- nameshift/nameshift/doctype/nameshift_selector/namershift_selector.js
+++ nameshift/nameshift/doctype/nameshift_selector/namershift_selector.js
@@ -1,31 +1,78 @@
-frappe.ui.form.on('NameShift Selector', {
-    app: function(frm) {
-        // Fetch DocTypes for the selected app
-        frappe.call({
-            method: 'nameshift.nameshift.doctype.nameshift_selector.nameshift_selector.get_doctypes',
-            args: {
-                app: frm.doc.app
-            },
-            callback: function(r) {
-                if(r.message) {
-                    frm.set_df_property('doctype', 'options', r.message);
-                }
-            }
-        });
-    },
-    doctype: function(frm) {
-        // Fetch fields for the selected DocType
-        frappe.call({
-            method: 'nameshift.nameshift.doctype.nameshift_selector.nameshift_selector.get_fields',
-            args: {
-                doctype: frm.doc.doctype
-            },
-            callback: function(r) {
-                if(r.message) {
-                    frm.set_df_property('field', 'options', r.message);
-                }
-            }
-        });
-    }
-});
+frappe.pages['nameshift_selector'].on_page_load = function(wrapper) {
+    var page = frappe.ui.make_app_page({
+        parent: wrapper,
+        title: 'NameShift Selector',
+        single_column: true
+    });
 
+    $(frappe.render_template('nameshift_selector', {})).appendTo(page.body);
+
+    page.add_field({
+        fieldname: 'select_doctype',
+        label: 'Select DocType',
+        fieldtype: 'Link',
+        options: 'DocType',
+        change: function() {
+            let doctype = this.get_value();
+            if (doctype) {
+                frappe.call({
+                    method: 'nameshift.nameshift.page.nameshift_selector.nameshift_selector.get_fields',
+                    args: {
+                        doctype: doctype
+                    },
+                    callback: function(r) {
+                        let fields = r.message || [];
+                        let field_select = page.fields_dict.select_field.$wrapper;
+                        field_select.empty();
+                        fields.forEach(field => {
+                            $('<option>', { value: field.fieldname, text: field.label }).appendTo(field_select);
+                        });
+                    }
+                });
+            }
+        }
+    });
+
+    page.add_field({
+        fieldname: 'select_field',
+        label: 'Select Field',
+        fieldtype: 'Select'
+    });
+
+    page.add_field({
+        fieldname: 'new_field_name',
+        label: 'New Field Name',
+        fieldtype: 'Data'
+    });
+
+    page.add_field({
+        fieldname: 'rename_field',
+        label: 'Rename Field',
+        fieldtype: 'Button',
+        click: function() {
+            let doctype = page.fields_dict.select_doctype.get_value();
+            let field = page.fields_dict.select_field.get_value();
+            let new_field_name = page.fields_dict.new_field_name.get_value();
+
+            if (doctype && field && new_field_name) {
+                frappe.call({
+                    method: 'nameshift.nameshift.page.nameshift_selector.nameshift_selector.rename_field',
+                    args: {
+                        doctype: doctype,
+                        field: field,
+                        new_field_name: new_field_name
+                    },
+                    callback: function(r) {
+                        if (r.message === 'success') {
+                            frappe.msgprint('Field renamed successfully');
+                        } else {
+                            frappe.msgprint('Error renaming field');
+                        }
+                    }
+                });
+            } else {
+                frappe.msgprint('Please fill in all fields');
+            }
+        }
+    });
+};
--- nameshift/nameshift/doctype/nameshift_selector/nameshift_selector.py
+++ nameshift/nameshift/doctype/nameshift_selector/nameshift_selector.py
@@ -1,34 +1,33 @@
 import frappe
-from frappe.model.document import Document
-
-class DocTypeSelector(Document):
-    def on_update(self):
-        if self.app and self.doctype and self.field and self.new_label:
-            self.update_field_label()
-
-    def update_field_label(self):
-        # Fetch DocType details
-        meta = frappe.get_meta(self.doctype)
-        field = next((f for f in meta.fields if f.fieldname == self.field), None)
-        
-        if field:
-            # Update field label
-            field.label = self.new_label
-            frappe.db.sql("""
-                UPDATE `tabDocField`
-                SET label=%s
-                WHERE parent=%s AND fieldname=%s
-            """, (self.new_label, self.doctype, self.field))
-            frappe.clear_cache(doctype=self.doctype)
-
-@frappe.whitelist()
-def get_doctypes(app):
-    doctypes = frappe.get_all('DocType', filters={'module': app}, pluck='name')
-    return doctypes
 
 @frappe.whitelist()
 def get_fields(doctype):
-    fields = frappe.get_meta(doctype).fields
-    fieldnames = [f.fieldname for f in fields]
-    return fieldnames
+    meta = frappe.get_meta(doctype)
+    fields = [{'fieldname': df.fieldname, 'label': df.label} for df in meta.fields]
+    return fields
 
+@frappe.whitelist()
+def rename_field(doctype, field, new_field_name):
+    try:
+        frappe.db.sql(f"""
+            UPDATE `tabDocField`
+            SET fieldname=%s
+            WHERE parent=%s AND fieldname=%s
+        """, (new_field_name, doctype, field))
+
+        # Also update in custom fields if exists
+        frappe.db.sql(f"""
+            UPDATE `tabCustom Field`
+            SET fieldname=%s
+            WHERE dt=%s AND fieldname=%s
+        """, (new_field_name, doctype, field))
+
+        # Commit the changes
+        frappe.db.commit()
+
+        # Reload the DocType to apply changes
+        frappe.reload_doctype(doctype)
+        return 'success'
+    except Exception as e:
+        frappe.log_error(frappe.get_traceback(), 'nameshift_selector.rename_field')
+        return str(e)
